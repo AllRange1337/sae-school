7f72400505f4ee55b794d977cb9f3a70cfe5f14b
AllRange1337 <allrange1337@gmail.com>
2016-04-18 19:02:30 +0200

    stuff

---
+++ digitalerBauernhof/src/Kuh.java
@@ -0,0 +1,86 @@
+public class Kuh
+{
+	private String name;
+	private int gewicht;
+	private int geburtsjahr;
+	private int gefressenHeu;
+	private double wasser;
+	private int heu;
+
+	public Kuh(String name, int gewicht, int geburtsjahr)
+	{
+		this.name = name;
+		this.gewicht = gewicht;
+		this.geburtsjahr = geburtsjahr;
+	}
+
+	public void setGeburtsjahr(int geburtsjahr)
+	{
+		if (geburtsjahr <= 2011)
+			this.geburtsjahr = geburtsjahr;
+	}
+
+	public int getGeburtsjahr()
+	{
+		return geburtsjahr;
+	}
+
+	public void setGewicht(int gewicht)
+	{
+		if (gewicht > 35 && gewicht <= 1000)
+		{
+			this.gewicht = gewicht;
+		}
+	}
+
+	public int getGewicht()
+	{
+		return gewicht;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void status()
+	{
+		StringBuilder result = new StringBuilder();
+		result.append("Status der Kuh ");
+		result.append(getName() + ":\n");
+		result.append("Gewicht : " + getGewicht() + "\n");
+		result.append("Geb.jahr: " + getGeburtsjahr());
+		System.out.println(result.toString());
+	}
+
+	public void feedCow(int menge)
+	{
+		if (menge > 0)
+		{
+			gefressenHeu += menge;
+		}
+	}
+	
+	public void gibWasser(double menge) {
+		if (menge > 0) wasser += menge;
+	}
+	public double melken()
+	{
+		double milch = 0;
+		int calcHeu = (heu / 5);
+		int calcWasser = (int) (wasser / 7.5);
+
+		if (calcHeu > 0 && calcWasser > 0) {
+			int mult = calcHeu > calcWasser ? calcWasser : calcHeu;
+			milch = 0.5 * mult;
+			wasser -= calcWasser * 7.5;
+			heu -= calcHeu * 5;
+		}
+		return milch;
+	}
+}
